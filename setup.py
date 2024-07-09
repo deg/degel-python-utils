04889a57b887638f945f8600b13ca9233142c3f5
David Goldfarb <deg@degel.com>
2024-07-09 12:13:36 +0300

    DRYer skeleton

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='degel-python-utils',
-    version='0.1.0',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    install_requires=[
-        # List your runtime dependencies here
-    ],
-    extras_require={
-        'dev': [
-            'pytest',  # example of a development dependency
-            # Add other development dependencies here
-        ],
-    },
-    python_requires='>=3.12',
-    author='David Goldfarb',
-    author_email='deg@degel.com',
-    description='Shared Python utilies from Degel Software Ltd.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/deg/degel-python-utils',
-    classifiers=[
-        'Programming Language :: Python :: 3.12',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-)
+setup()
